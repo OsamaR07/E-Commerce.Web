ddc5bbd7e704ff778e7b3e445b5661e2516ff392
OsamaR07 <osama.ragab.huissen@gmail.com>
2025-06-19 10:27:19 +0300

    Part 08 Product Module -  Product Controller

--- Infrastructure/Presentation/Controllers/ProductsController.cs
+++ Infrastructure/Presentation/Controllers/ProductsController.cs
@@ -1,8 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
+using Shared.DataTransferObjects;
 
 namespace Presentation.Controllers;
 
-public class ProductsController(IServiceManager _serviceManager)
+[ApiController]
+[Route("api/[controller]")] // BaseUrl/api/Products
+public class ProductsController(IServiceManager _serviceManager) : ControllerBase
 {
-    
+    // Get All Products
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllProducts()
+        => Ok(await _serviceManager.ProductService.GetAllProductsAsync());
+
+    // Get Product By Id
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<ProductDto>> GetProductById(int id)
+        => Ok(await _serviceManager.ProductService.GetProductByIdAsync(id));
+
+    // Get All Types
+    [HttpGet("types")]
+    public async Task<ActionResult<IEnumerable<TypeDto>>> GetTypes()
+        => Ok(await _serviceManager.ProductService.GetAllTypesAsync());
+
+    // Get All Brands
+    [HttpGet("brands")]
+    public async Task<ActionResult<IEnumerable<BrandDto>>> GetBrands()
+        => Ok(await _serviceManager.ProductService.GetAllBrandsAsync());
 }
